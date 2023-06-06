5fc8e9056e23ab81ca9edf417f6a193b0db5f6c2
Rahma_salah <58772969+Rahma-salah44@users.noreply.github.com>
2023-06-06 15:59:09 +0200

    singleton poc Demo

---
+++ DesignPatterns/SingletonDP/Program.cs
@@ -0,0 +1,12 @@
+﻿using SingletonDP;
+
+Console.WriteLine("***Singleton Pattern Demo***\n");
+Console.WriteLine("Trying to create instance s1.");
+singleton s1 = singleton.Instance;
+Console.WriteLine("Trying to create instance s2.");
+singleton s2 = singleton.Instance;
+if (s1 == s2)
+    Console.WriteLine("Only one instance exists.");
+else
+    Console.WriteLine("Different instances exist.");
+
